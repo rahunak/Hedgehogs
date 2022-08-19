3901d97a49f28c3793157e98832ea6b0dacac0bc
Zaiko Euheni <rahunak@yandex.by>
2022-08-20 00:49:38 +0300

    feat: did markup and some styles

--- src/components/scripts/burger.js
+++ src/components/scripts/burger.js
@@ -1,1 +1,6 @@
-console.log('scripts enable')
+const burger = document.querySelector('#burger');
+function toggleBurger() {
+    console.log('scripts enable');
+    document.querySelector('.burger__content').classList.toggle('toggle_burger');
+}
+burger.addEventListener('click', toggleBurger);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import './components/styles/common.scss';
-
 import './components/styles/fonts.scss';
+import './components/styles/common.scss';
+import './components/styles/background.scss';
+import './components/styles/header.scss';
+import './components/styles/main.scss';
+import './components/styles/benefits.scss';
+import './components/styles/footer.scss';
 
+import './components/styles/burger.scss';
 import './components/scripts/burger';
