ab7d8b195cb801d5335c649c6fd6ed5474a3951d
Zaiko Euheni <rahunak@yandex.by>
2022-08-20 15:20:21 +0300

    feat: add dropdown

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import './components/styles/fonts.scss';
 import './components/styles/common.scss';
 import './components/styles/background.scss';
 import './components/styles/header.scss';
+import './components/styles/dropdown_header.scss';
 import './components/styles/main.scss';
 import './components/styles/benefits.scss';
 import './components/styles/footer.scss';
 
 import './components/styles/burger.scss';
 import './components/scripts/burger';
+import './components/scripts/dropdown_header';
