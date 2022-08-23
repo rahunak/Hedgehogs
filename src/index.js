6d47da4cd5cb1bb5142ad8293beb4ddbb93b1e6c
Zaiko Euheni <rahunak@yandex.by>
2022-08-23 22:33:36 +0300

    feat: did mobile version

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import './components/styles/header.scss';
 import './components/styles/dropdown_header.scss';
 import './components/styles/main.scss';
 import './components/styles/benefits.scss';
+import './components/styles/tiles.scss';
+import './components/styles/order.scss';
+import './components/styles/other.scss';
+import './components/styles/burger.scss';
 import './components/styles/footer.scss';
 
-import './components/styles/burger.scss';
 import './components/scripts/burger';
 import './components/scripts/dropdown_header';
